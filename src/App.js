d26281327b6aaa720da27d96c462ec1e24f65809
naeem4633 <ahmedn3700@gmail.com>
2023-07-10 07:40:38 +0500

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <></>
+    <Router>
+      <div className="App">
+        <div className="App-body">
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
